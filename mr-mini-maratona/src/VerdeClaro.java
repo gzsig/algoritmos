ddbfe43412d23af7c0146a433fc4a7eb2effd3be
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-28 15:54:06 -0300

    verde claro resolvido

--- mr-mini-maratona/src/VerdeClaro.java
+++ mr-mini-maratona/src/VerdeClaro.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class VerdeClaro {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int usrIn, div = 1;
+        double res = 0;
+        System.out.println("Numero de frações?");
+        usrIn = keyboard.nextInt();
+        for (int i = 0; i < usrIn; i++){
+          res += (1/Math.pow(div, 3));
+          div = div + 2;
+        }
+        System.out.println(res);
+    }
 }
