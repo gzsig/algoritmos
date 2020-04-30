763a54b73ba1732abefc91840d50164c0b46f891
Gabriel Zsigmond <gaazsig@gmail.com>
2020-04-30 14:41:44 -0300

    Atividade 1 seems to be complete

--- TI2A-Estrutura-de-Dados/atividade-I/src/Main.java
+++ TI2A-Estrutura-de-Dados/atividade-I/src/Main.java
@@ -19,11 +19,12 @@ public class Main {
       command = keyboard.nextInt();
 
     }
-    playlist.print();
+    System.out.println("Au revoir, à bientôt");
     keyboard.close();
   }
 
   public static boolean evaluateCommand(Playlist playlist, int command, Scanner keyboard) {
+    String title;
     switch (command) {
       case 1:
         System.out.println("Vamos inserir uma musica nova :)");
@@ -33,21 +34,24 @@ public class Main {
         System.out.println("Oba, nova musica na playlist");
         break;
       case 2:
-        System.out.println("Bora escutar um som?");
-        playlist.play("mus");
+        System.out.println("Bora escutar um som?\nComo chama a musica que vc quer escutar?");
+        keyboard.nextLine();
+        title = keyboard.nextLine();
+        playlist.play(title);
         break;
       case 3:
-        System.out.println("Cansou? Então bora excluir!");
+        System.out.println("Cansou? Então bora excluir!\nComo chama a musica que vc quer excluir?");
+        keyboard.nextLine();
+        title = keyboard.nextLine();
+        playlist.remove(title);
         break;
       case 4:
         System.out.println("Hora da verdade... Vamos ver o que você anda escutando");
-        break;
-      case 5:
-        System.out.println("Au revoir, à bientôt");
+        playlist.topSongs();
         break;
 
       default:
-        System.out.println("Comando não encontrado.");
+        System.out.println("Comando não encontrado. Mas aqui estão todas a musicas :)");
         playlist.print();
         break;
     }
--- TI2A-Estrutura-de-Dados/atividade-I/src/Playlist.java
+++ TI2A-Estrutura-de-Dados/atividade-I/src/Playlist.java
@@ -16,39 +16,166 @@ public class Playlist {
       tmp.left = last;
       last = tmp;
     }
+    alphabetical();
   }
 
   public Node search(String title) {
     Node searching = first;
-    System.out.println("entrei");
     while (searching != null) {
-      System.out.println(searching.data.title);
       if (searching.data.title.equals(title)) {
-        System.out.println("ACHEI");
         return searching;
       }
       searching = searching.right;
     }
-    System.out.println("sai");
     return searching;
   }
 
   public void play(String title) {
     Node song = search(title);
-    if (song != null){
-      song.data.playTimes ++;
+    if (song != null) {
+      song.data.playTimes++;
     } else {
       System.out.println("Xii, não encontrei essa ai");
     }
   }
 
+  public boolean remove(String title) {
+    Node song = search(title);
+    if (song != null) {
+      if (song.left == null && song.right == null) {
+        first = null;
+        last = null;
+        return true;
+      } else if (song.left == null) {
+        song.right.left = null;
+        first = song.right;
+        return true;
+      } else if (song.right == null) {
+        song.left.right = null;
+        last = song.left;
+        return true;
+      } else {
+        Node songPrev = song.left;
+        Node songright = song.right;
+        songPrev.right = song.right;
+        songright.left = song.left;
+        return true;
+      }
+    } else {
+      System.out.println("Xii, não encontrei essa ai");
+      return false;
+    }
+  }
+
+  public void topSongs() {
+    Node iterate = first;
+    Song top1 = null, top2 = null, top3 = null, top4 = null, top5 = null;
+    int play1 = 0, play2 = 0, play3 = 0, play4 = 0, play5 = 0;
+    while (iterate != null) {
+      if (iterate.data.playTimes > play1) {
+        play1 = iterate.data.playTimes;
+        top1 = iterate.data;
+        iterate = iterate.right;
+        continue;
+      } else if (iterate.data.playTimes > play2) {
+        play2 = iterate.data.playTimes;
+        top2 = iterate.data;
+        iterate = iterate.right;
+        continue;
+      } else if (iterate.data.playTimes > play3) {
+        play3 = iterate.data.playTimes;
+        top3 = iterate.data;
+        iterate = iterate.right;
+        continue;
+      } else if (iterate.data.playTimes > play4) {
+        play4 = iterate.data.playTimes;
+        top4 = iterate.data;
+        iterate = iterate.right;
+        continue;
+      } else if (iterate.data.playTimes > play5) {
+        play5 = iterate.data.playTimes;
+        top5 = iterate.data;
+        iterate = iterate.right;
+        continue;
+      }
+      iterate = iterate.right;
+    }
+    if (top1 == null && top2 == null && top3 == null && top4 == null && top5 == null){
+      System.out.println("Nenhuma musica foi tocada :/");
+    }
+    if (top1 != null) {
+      System.out.print("TOP 1 – Titulo: " + top1.title + " – ");
+      System.out.print("Artista: " + top1.artist + " – ");
+      System.out.print("Genero: " + top1.genre + " – ");
+      System.out.println("Vezes tocada: " + top1.playTimes + "\n");
+    }
+    if (top2 != null) {
+      System.out.print("TOP 2 – Titulo: " + top2.title + " – ");
+      System.out.print("Artista: " + top2.artist + " – ");
+      System.out.print("Genero: " + top2.genre + " – ");
+      System.out.println("Vezes tocada: " + top2.playTimes + "\n");
+    }
+    if (top3 != null) {
+      System.out.print("TOP 3 – Titulo: " + top3.title + " – ");
+      System.out.print("Artista: " + top3.artist + " – ");
+      System.out.print("Genero: " + top3.genre + " – ");
+      System.out.println("Vezes tocada: " + top3.playTimes + "\n");
+    }
+    if (top4 != null) {
+      System.out.print("TOP 4 – Titulo: " + top4.title + " – ");
+      System.out.print("Artista: " + top4.artist + " – ");
+      System.out.print("Genero: " + top4.genre + " – ");
+      System.out.println("Vezes tocada: " + top4.playTimes + "\n");
+    }
+    if (top5 != null) {
+      System.out.print("TOP 5 – Titulo: " + top5.title + " – ");
+      System.out.print("Artista: " + top5.artist + " – ");
+      System.out.print("Genero: " + top5.genre + " – ");
+      System.out.println("Vezes tocada: " + top5.playTimes + "\n");
+    }
+  }
+
+  public void alphabetical() {
+    for (int i = 0; i < size() - 1; i++) {
+      for (int j = 0; j < size() - i - 1; j++) {
+        if (get(j).data.title.compareTo(get(j + 1).data.title) > 0) {
+          swap(get(j), get(j + 1));
+        }
+      }
+    }
+  }
+
+  public void swap(Node swap1, Node swap2) {
+    Song tmp = swap1.data;
+    swap1.data = swap2.data;
+    swap2.data = tmp;
+  }
+
+  public int size() {
+    Node count = first;
+    int cont = 0;
+    while (count != null) {
+      cont++;
+      count = count.right;
+    }
+    return cont;
+  }
+
+  public Node get(int index) {
+    Node atIndex = first;
+    for (int i = 0; i < index; i++) {
+      atIndex = atIndex.right;
+    }
+    return atIndex;
+  }
+
   public void print() {
     Node toPrint = first;
     while (toPrint != null) {
-      System.out.println("Titulo: " + toPrint.data.title);
-      System.out.println("Artista: " + toPrint.data.artist);
-      System.out.println("Genero: " + toPrint.data.genre);
-      System.out.println("Vezes tocada: " + toPrint.data.playTimes);
+      System.out.print("Titulo: " + toPrint.data.title + " – ");
+      System.out.print("Artista: " + toPrint.data.artist + " – ");
+      System.out.print("Genero: " + toPrint.data.genre + " – ");
+      System.out.println("Vezes tocada: " + toPrint.data.playTimes + "\n");
       toPrint = toPrint.right;
     }
   }
