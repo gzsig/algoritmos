6df07db0f85081ec3c7b8806e956da3307a5d7b0
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-15 12:04:50 -0300

    Main file testing Pilha

---
+++ TI2A-Estrutura-de-Dados/aula08-EaD/srcPolishNotation/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Pilha<Double> test = new Pilha<Double>();
+    test.push(4.0);
+    test.push(8.0);
+    test.push(2.0);
+    test.push(6.0);
+    test.imprimir();
+  }
+}
