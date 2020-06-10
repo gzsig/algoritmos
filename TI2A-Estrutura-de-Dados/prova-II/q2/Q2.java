8b12dd2fab3923b01af7bba79e1b5f7b27477d82
Gabriel Zsigmond <gaazsig@gmail.com>
2020-06-10 19:42:49 -0300

    Explaining why it doesn't work

--- TI2A-Estrutura-de-Dados/prova-II/q2/Q2.java
+++ TI2A-Estrutura-de-Dados/prova-II/q2/Q2.java
@@ -5,7 +5,7 @@ public class Q2 {
 
   public static void main(String[] args) {
     System.out.println(pell(10)); //2378
-    System.out.println(pell(197)); //Loop infinito
+    System.out.println(pell(197)); //Loop infinito, pois, apos algumas chamadas recursivas, o valor  excede  o valor maximo de uma variavel do tipo int e assim perde precisão nunca alcançando a condição de parada.
   }
 
   public static int pell(int x) {
