46ba02c07a7fda9b0c9287363fec4cdec4672d62
Gabriel Zsigmond <gaazsig@gmail.com>
2020-06-10 14:26:58 -0300

    Final assessment II question 3 ok

---
+++ TI2A-Estrutura-de-Dados/prova-II/q3/Q3.java
@@ -0,0 +1,25 @@
+/**
+ * Q3
+ */
+public class Q3 {
+
+  public static void main(String[] args) {
+    System.out.println(somatoryNtoZeroRec(6));
+    System.out.println(somatoryNtoZeroLoop(6));
+  }
+
+  public static int somatoryNtoZeroRec(int n) {
+    if (n <= 0)
+      return 0;
+    return n + somatoryNtoZeroRec(n - 1);
+  }
+
+  public static int somatoryNtoZeroLoop(int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  // Essa função faz o somatorio de 0 até n
+}
