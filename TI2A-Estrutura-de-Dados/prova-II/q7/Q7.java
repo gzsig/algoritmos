27faabefb07a9724fa2ca0dce9b416390d4caeac
Gabriel Zsigmond <gaazsig@gmail.com>
2020-06-11 15:02:33 -0300

    Final assessment II question 7 Bug Fix

--- TI2A-Estrutura-de-Dados/prova-II/q7/Q7.java
+++ TI2A-Estrutura-de-Dados/prova-II/q7/Q7.java
@@ -4,16 +4,18 @@
 public class Q7 {
 
   public static void main(String[] args) {
-    System.out.println(ackermann(10, 4));
+    System.out.println(ackermann(3, 4));
   }
 
   public static int ackermann(int m, int n) {
-    if (m == 0)
+    if (m == 0) {
       return n + 1;
-    if (m > 0 && n == 0)
-      return ackermann(m - 1, n);
-    if (m > 0 && n > 0)
+    } else if (m > 0 && n == 0) {
+      return ackermann(m - 1, 1);
+    } else if (m > 0 && n > 0) {
       return ackermann(m - 1, ackermann(m, n - 1));
-    return 0;
+    } else {
+      return 0;
+    }
   }
 }
