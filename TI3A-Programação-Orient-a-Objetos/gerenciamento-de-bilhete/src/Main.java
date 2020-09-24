3486a703bb85dbc9b5e3d72e08bf508332aaf679
Gabriel Zsigmond <gaazsig@gmail.com>
2020-09-23 23:36:39 -0300

    Start of UI

---
+++ TI3A-Programação-Orient-a-Objetos/gerenciamento-de-bilhete/src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+  public static void main(String[] args) {
+    String credentials;
+    credentials = JOptionPane.showInputDialog(null, "Senha ou CPF ou Sair");
+  }
+}
